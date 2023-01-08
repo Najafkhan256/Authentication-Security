c782944fa8d2adc05018fa23c71aa0336646cf19
Najaf <najafkhan6625@gmail.com>
2023-01-08 16:30:30 +0500

    Add Envoirment vars

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
-
+console.log(process.env.API_KEY);
 app.set("view engine", "ejs");
 app.use(
   bodyParser.urlencoded({
@@ -26,9 +26,11 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret";
 /* Encrypting the password field in the database. */
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"]
+});
 
 // user model and collection
 const User = new mongoose.model("User", userSchema);
